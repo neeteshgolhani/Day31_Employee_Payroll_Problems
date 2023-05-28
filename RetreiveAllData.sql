b5dd49a1e71011dad625bc5ca0d80d1186f47f12
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-05-28 19:44:13 +0530

    Added:Ability to retrieve all the employee payroll data that is added to payroll service database

---
+++ RetreiveAllData.sql
@@ -0,0 +1,12 @@
+mysql> use payroll_service;
+Database changed
+mysql> SELECT * FROM employee_payroll;
++----+---------+----------+------------+
+| id | name    | salary   | date       |
++----+---------+----------+------------+
+|  1 | Neetesh | 30000.00 | 2021-06-24 |
+|  2 | Anya    | 23000.00 | 2022-02-23 |
+|  3 | Sreya   | 45000.00 | 2022-04-23 |
+|  4 | Anit    | 34500.00 | 2022-10-22 |
++----+---------+----------+------------+
+4 rows in set (0.00 sec)
