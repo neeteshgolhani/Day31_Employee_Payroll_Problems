8c3741a08de8eca5b0fc66cd023a0f86fe3aacca
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-06-01 19:58:12 +0530

    Ability to create employee payroll data in the payroll service database as part of CRUD Operation - Use payroll_service database in

--- InsertData.sql
+++ InsertData.sql
@@ -1,18 +1,32 @@
 mysql> use payroll_service;
 Database changed
-mysql> insert into employee_payroll(name,salary,date) values('Neetesh',30000,'24-06-2021'),('Anya',23000,'23-02-2022'),('Sreya',45000,'23-04-2022'),('Anit',34500,'22-10-2022');
-ERROR 1292 (22007): Incorrect date value: '24-06-2021' for column 'date' at row 1
-mysql> insert into employee_payroll (name, salary, date) values ('Neetesh', 30000, '2021-06-24'), ('Anya', 23000, '2022-02-23'), ('Sreya', 45000, '2022-04-23'), ('Anit', 34500, '2022-10-22');
-Query OK, 4 rows affected (0.02 sec)
-Records: 4  Duplicates: 0  Warnings: 0
+mysql> insert into employee_payroll(name,salary,date) values('Bill',5000,'2017-06-15');
+Query OK, 1 row affected (0.01 sec)
+
+mysql> insert into employee_payroll(name,salary,date) values('Mike',8000,'2018-01-10');
+Query OK, 1 row affected (0.01 sec)
+
+mysql> insert into employee_payroll(name,salary,date) values('Dustin',4000,'2018-02-20');
+Query OK, 1 row affected (0.01 sec)
+
+mysql> insert into employee_payroll(name,salary,date) values('Millie',10000,'2018-05-1');
+Query OK, 1 row affected (0.01 sec)
+
+mysql> insert into employee_payroll(name,salary,date) values('Jack','9000','2019-10-05');
+Query OK, 1 row affected (0.01 sec)
+
+mysql> insert into employee_payroll(name,salary,date) values('Vecna',12000,'2020-03-25');
+Query OK, 1 row affected (0.01 sec)
 
 mysql> SELECT * FROM employee_payroll;
-+----+---------+----------+------------+
-| id | name    | salary   | date       |
-+----+---------+----------+------------+
-|  1 | Neetesh | 30000.00 | 2021-06-24 |
-|  2 | Anya    | 23000.00 | 2022-02-23 |
-|  3 | Sreya   | 45000.00 | 2022-04-23 |
-|  4 | Anit    | 34500.00 | 2022-10-22 |
-+----+---------+----------+------------+
-4 rows in set (0.00 sec)
++----+--------+----------+------------+
+| id | name   | salary   | date       |
++----+--------+----------+------------+
+|  5 | Bill   |  5000.00 | 2017-06-15 |
+|  6 | Mike   |  8000.00 | 2018-01-10 |
+|  7 | Dustin |  4000.00 | 2018-02-20 |
+|  8 | Millie | 10000.00 | 2018-05-01 |
+|  9 | Jack   |  9000.00 | 2019-10-05 |
+| 10 | Vecna  | 12000.00 | 2020-03-25 |
++----+--------+----------+------------+
+6 rows in set (0.00 sec)
